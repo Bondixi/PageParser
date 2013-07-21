d11c580eaa15d7d2704b5a5af19b1e05aa438d74
bondixi <bondixi0@gmail.com>
2013-07-21 15:30:02 +0400

    all

---
+++ PageParser/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace PageParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Source = new System.Windows.Forms.TextBox();
+            this.RegExpSrc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ResultGrid = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ResultGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Source
+            // 
+            this.Source.AcceptsReturn = true;
+            this.Source.AllowDrop = true;
+            this.Source.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Source.Location = new System.Drawing.Point(12, 12);
+            this.Source.MaxLength = 3276700;
+            this.Source.Multiline = true;
+            this.Source.Name = "Source";
+            this.Source.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Source.Size = new System.Drawing.Size(416, 88);
+            this.Source.TabIndex = 0;
+            // 
+            // RegExpSrc
+            // 
+            this.RegExpSrc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RegExpSrc.Location = new System.Drawing.Point(489, 10);
+            this.RegExpSrc.Name = "RegExpSrc";
+            this.RegExpSrc.Size = new System.Drawing.Size(291, 20);
+            this.RegExpSrc.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(435, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "RegExp:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(438, 73);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(342, 27);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // ResultGrid
+            // 
+            this.ResultGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResultGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ResultGrid.Location = new System.Drawing.Point(12, 106);
+            this.ResultGrid.Name = "ResultGrid";
+            this.ResultGrid.Size = new System.Drawing.Size(768, 402);
+            this.ResultGrid.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(434, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Range {1-10} or custom [regexp]";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(705, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Launch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 520);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ResultGrid);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RegExpSrc);
+            this.Controls.Add(this.Source);
+            this.Name = "Form1";
+            this.Text = "PageParser";
+            ((System.ComponentModel.ISupportInitialize)(this.ResultGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Source;
+        private System.Windows.Forms.TextBox RegExpSrc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DataGridView ResultGrid;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PageParser/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PageParser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ResultGrid.Columns.Clear();
+            
+            //ResultGrid.Columns.Add("Index", "N");
+            //DataGridViewTextBoxCell cell = new DataGridViewTextBoxCell();
+            //cell.Value = cell.RowIndex;
+            //ResultGrid.Columns["Index"].CellTemplate = cell;
+            //ResultGrid.DataSource = Results.ToList();
+            //ResultGrid.DataSource = Generate(words).ToList();
+            //ResultGrid.Columns.Remove("Откуда");
+        }
+    }
+}
